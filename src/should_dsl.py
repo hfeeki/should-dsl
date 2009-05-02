6ebe27c62b255e6ea2182bfef5f83205c976d439
rmanhaes <rmanhaes@89f1475a-30cd-11de-a996-7ff70a941223>
2009-05-02 22:19:15 +0000

    should-dsl, alternative version

--- src/should_dsl.py
+++ src/should_dsl.py
@@ -1,119 +1,109 @@
-'''
-    This is a simple experiment with internal DSLs
-    using Python Language.
-    The goal is to write some kind of BDD.
+#coding: utf-8
 
-
-    Here is the point to try make everything
-    as transparent as possible
-    ``from should_dsl import DSLObject as _``
-
-    Take a look at the doctests (doctests folder)
-'''
-
-class DSLCommon(object):
-    def __init__(self, value, negate=False):
-        self._value = value
+class Should(object):
+    
+    def __init__(self, negate=False):
         self._negate = negate
-
-    def _negate_or_not(self, value):
+        self._is_thrown_by = False
+    
+    def _evaluate(self, value):
         if self._negate:
             return not value
         return value
-
-
-class DSLObject(object):
-    def __init__(self, value):
-        self._value = value
-
-    @property
-    def should_be(self):
-        return Should(self._value, negate=False)
+    
+    def _negate_str(self):
+        if not self._negate:
+            return 'not '
+        return ''
+    
+    def __rlshift__(self, lvalue):
+        self._lvalue = lvalue
+        if not self._has_rvalue:
+            return self._check_assertion()
+        return self
+    
+    def __rshift__(self, rvalue):
+        self._rvalue = rvalue
+        return self._check_assertion()
     
     @property
-    def should_not_be(self):
-        return Should(self._value, negate=True)
-
-    def should_have(self, value):
-        return Should(self._value, negate=False).have(value)
-
-    def should_not_have(self, value):
-        return Should(self._value, negate=True).have(value)
-
-
-class ShouldNotSatisfied(Exception):
-    '''it's raised when some should is not satisfied'''
-
-
-class Should(DSLCommon):
-    @property
-    def success(self):
-        return True
-
-    def fail(self, message):
-        raise ShouldNotSatisfied(message)
-        return False
-
-    @property
-    def none(self):
-        if self._negate_or_not(self._value == None):
-            return self.success
-        negate = ''
-        if self._negate == False:
-            negate = 'not '
-        return self.fail("%s is %sNone" % (self._value, negate))
-
+    def equal_to(self):
+        self._func = lambda x, y: x == y
+        self._error_message = lambda x, y: '%s is %sequal to %s' % (x, self._negate_str(), y)
+        self._has_rvalue = True
+        return self
+    
     @property
     def true(self):
-        if self._negate_or_not(self._value):
-            return self.success
-        negate = ''
-        if self._negate == False:
-            negate = 'not '
-        return self.fail("%s is %sTrue" % (self._value, negate))
-
+        self._func = lambda x: x is True
+        self._error_message = lambda x: '%s is %sTrue' % (x, self._negate_str())
+        self._has_rvalue = False
+        return self
+    
     @property
     def false(self):
-        if self._negate_or_not(self._value) == False:
-            return self.success
-        negate = ''
-        if self._negate == False:
-            negate = 'not '
-        return self.fail("%s is %sFalse" % (self._value, negate))
+        self._func = lambda x: x is False
+        self._error_message = lambda x: '%s is %sFalse' % (x, self._negate_str())
+        self._has_rvalue = False
+        return self
+    
+    @property
+    def none(self):
+        self._func = lambda x: x == None
+        self._error_message = lambda x: '%s is %sNone' % (x, self._negate_str())
+        self._has_rvalue = False
+        return self
+    
+    @property
+    def into(self):
+        self._func = lambda item, container: item in container
+        self._error_message = lambda item, container: '%s is %sinto %s' % (item, self._negate_str(), container)
+        self._has_rvalue = True
+        return self 
+    
+    @property
+    def have(self):
+        self._func = lambda container, item: item in container
+        self._error_message = lambda container, item: '%s does %shave %s' % (container, self._negate_str(), item)
+        self._has_rvalue = True
+        return self
+    
+    @property
+    def thrown_by(self):
+        def check_exception(exception, callable, *args, **kw):
+            try:
+                callable(*args, **kw)
+                return False
+            except exception:
+                return True
+        self._func = check_exception
+        self._error_message = lambda exception, callable: '%s is %sthrown by %s' % (exception, self._negate_str(), callable)
+        self._has_rvalue = True
+        self._is_thrown_by = True
+        return self
+    
+    def _check_assertion(self):
+        if self._has_rvalue:
+            evaluation = None 
+            if self._is_thrown_by and self._rvalue.__class__ in (tuple, list, dict) and len(self._rvalue) > 1:
+                evaluation = self._evaluate(self._func(self._lvalue, self._rvalue[0], *self._rvalue[1:]))
+            else:
+                evaluation = self._evaluate(self._func(self._lvalue, self._rvalue)) 
+            if not evaluation:
+                raise ShouldNotSatisfied, self._error_message(self._lvalue, self._rvalue)
+            else:
+                return True
+        else:
+            if not self._evaluate(self._func(self._lvalue)):
+                raise ShouldNotSatisfied, self._error_message(self._lvalue)
+            else:
+                return True
+            
+            
+class ShouldNotSatisfied(Exception):
+    pass
 
-    def equal_to(self, value):
-        if self._negate_or_not(value == self._value):
-            return self.success
-        negate = ''
-        if self._negate == False:
-            negate = 'not '
-        return self.fail("%s is %sequal to %s" % (self._value, negate, value))
-
-    def into(self, container):
-        if self._negate_or_not(self._value in container):
-            return self.success
-        negate = ''
-        if self._negate == False:
-            negate = 'not '
-        return self.fail("%s is %sinto %s" % (self._value, negate, container))
-
-    def have(self, value):
-        if self._negate_or_not(value in self._value):
-            return self.success
-        negate = ''
-        if self._negate == False:
-            negate = 'not '
-        return self.fail("%s does %shave %s" % (self._value, negate, value))
-
-    def thrown_by(self, callable_object, *args, **kw):
-        try:
-            callable_object(*args, **kw)
-            if self._negate == False:
-                return self.fail("%s is not thrown by %s" % (str(self._value),
-                                                             callable_object))
-        except self._value:
-            if self._negate == True:
-                return self.fail("%s is thrown by %s" % (str(self._value),
-                                                             callable_object))
-        return True
- 
+should_be = Should(negate=False)
+should_not_be = Should(negate=True)
+should_have = Should(negate=False).have
+should_not_have = Should(negate=True).have
